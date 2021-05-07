f78f75783afd448bbf108ea08c6aa8afd2e35379
jeromeolivier <jerome.olivier@icloud.com>
2021-05-07 11:25:03 -0400

    create and edit functionality working

--- src/main/java/com/example/journalApp/subD/JournalController.java
+++ src/main/java/com/example/journalApp/subD/JournalController.java
@@ -1,11 +1,14 @@
 package com.example.journalApp.subD;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Date;
+import java.time.Instant;
+import java.util.Optional;
 
 @Controller
 public class JournalController {
@@ -14,20 +17,34 @@ public class JournalController {
     JournalRepository journalRepository;
 
     @GetMapping(value = "/add")
-    public String addEntry(String title, Date date, String body) {
+    public String addEntry(String title, String body, ModelMap model) {
         JournalEntry journalEntry = new JournalEntry();
         journalEntry.setTitle(title);
-        journalEntry.setDate(java.util.Calendar.getInstance().getTime());
+        journalEntry.setDate(java.util.Date.from(Instant.now()));
         journalEntry.setBody(body);
         journalRepository.save(journalEntry);
+        model.addAttribute("journalEntry", journalRepository.findAll(Sort.by("number").descending()));
 
         return "journalView";
     }
 
     @GetMapping("/startApp")
     public String listPreviousEntries(ModelMap model) {
-        model.addAttribute("journalEntry", journalRepository.findAll());
+        model.addAttribute("journalEntry", journalRepository.findAll(Sort.by("number").descending()));
         return "journalView";
+    }
+
+    @GetMapping(value = "/edit")
+    public String editEntry(Integer entryNumber, ModelMap model) {
+//        entryNumber = Integer.parseInt(entryString);
+        Optional<JournalEntry> searchResult = journalRepository.findById(entryNumber);
+        if (searchResult.isPresent()) {
+            JournalEntry journalEntry = searchResult.get();
+            model.addAttribute("entryToUpdate", journalEntry);
+            return "journalEdit";
+        } else {
+            return "noEntryFound";
+        }
     }
 }
 
--- src/main/java/com/example/journalApp/subD/JournalRepository.java
+++ src/main/java/com/example/journalApp/subD/JournalRepository.java
@@ -1,7 +1,9 @@
 package com.example.journalApp.subD;
 
 import com.example.journalApp.subD.JournalEntry;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
 public interface JournalRepository extends CrudRepository<JournalEntry, Integer> {
+    Object findAll(Sort number);
 }
